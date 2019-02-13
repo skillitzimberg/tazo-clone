a69f7d872c087e13d42aa27f76e10a30a5271d16
Quinn Miller and Scott Bergler <quinn@>
2019-02-13 09:57:45 -0800

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App(){
+  return (
+    <div>
+      app works
+    </div>
+  );
+}
+
+export default App;
